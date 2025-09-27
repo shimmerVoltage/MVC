92688cd3cf41436f93a0032c7e48c4a386adbf4e
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-09-27 21:32:44 +0300

    Add project files.

---
+++ ContosoUniversity/Data/UniversityContext.cs
@@ -0,0 +1,20 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class UniversityContext:DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options):base(options){}
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Students");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollments");
+            modelBuilder.Entity<Course>().ToTable("Courses");
+        }
+    }
+}
