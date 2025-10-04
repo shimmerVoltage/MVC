465c3e54842fbb4084110f6d1ff2fbc37812b3f7
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-10-04 20:35:14 +0300

    Pagination MVC DONE

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -20,12 +20,22 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students
-        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
         {
+            ViewData["CurrentSort"] = sortOrder;
             ViewData["NameSortParam"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewData["DateSortParam"] = sortOrder == "date" ? "date_desc" : "date";
 
-            ViewData["CurrentFilter"] = searchString;
+            if(searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }    
+
+                ViewData["CurrentFilter"] = searchString;
 
             IQueryable<Student> students = from s in _context.Students select s;
 
@@ -42,7 +52,9 @@ namespace ContosoUniversity.Controllers
                 default:            students = students.OrderBy(s => s.LastName); break;
             }
 
-            return View(await students.AsNoTracking().ToListAsync());
+            int pageSize = 3;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
+            //return View(await students.AsNoTracking().ToListAsync());
             //return View(await _context.Students.ToListAsync());
         }
 
